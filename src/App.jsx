753d41a92056f8555ecf06098f94239dc08d0e7e
Charuhas patil <charuhas1602@gmail.com>
2024-02-12 22:42:49 +0530

    Star Rating app completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { FaRegStar } from "react-icons/fa";
 
+const arrayLength = 5;
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentRating, setCurrentRating] = useState(0);
+  const [mouseOver, setMouseOver] = useState(0);
 
+  const handleclick = (id) => {
+    console.log("Mouse Click", id);
+    setCurrentRating(id);
+  };
+  const handleMouseEnter = (id) => {
+    console.log("Mouse Enter", id);
+    setMouseOver(id);
+  };
+  const handleMouseLeave = (id) => {
+    console.log("Mouse Leave", id);
+    setMouseOver(currentRating);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Star⭐Rating</h1>
+      {[...Array(arrayLength)].map((_, index) => {
+        index += 1;
+        return (
+          <>
+            <FaRegStar
+              style={{
+                fontSize: "70px",
+                color: index <= (mouseOver || currentRating) ? "red" : "",
+              }}
+              key={index}
+              onMouseEnter={() => handleMouseEnter(index)}
+              onClick={() => handleclick(index)}
+              onMouseLeave={() => handleMouseLeave(index)}
+            ></FaRegStar>
+          </>
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
